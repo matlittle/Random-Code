d67f79f595fe9a8a887d71bbd3cd029f7cb6663e
matlittle <matlittle727@gmail.com>
2018-01-18 16:04:14 -0800

    Trying to make everything tail recursive

--- Quick-Sort/sort.js
+++ Quick-Sort/sort.js
@@ -1,63 +1,87 @@
 var qCounter = 0;
 var bCounter = 0;
 
-function quickSort(arr, pvt = true) {
+function quickSort(arr, low, high) {
 
-    if (arr.length <= 1) {
-        return arr;
-    }
+	const pivotIndex = Math.floor(arr.length / 2);
 
-    const mid = (pvt ? arr[0] : arr[arr.length - 1]);
-    let lowArr = [];
-    let highArr =[];
+	let i = low;
+	let j = high;
 
-    for(let i = 0; i < arr.length; i++) {
-        qCounter++;
+	while(i < j) {
 
-        if(arr[i] <= mid) {
-            lowArr.push( arr[i] );
-        } else {
-            highArr.push( arr[i] );
-        }
-    }
+		while(arr[i] < arr[pivotIndex]) {
+			i++;
+		}
 
-    return quickSort( lowArr, !pvt ).concat( quickSort( highArr, pvt ) );
+		while(arr[j] > arr[pivotIndex]) {
+			j--;
+		}
 
+		if(i < j) {
+			if(i === pivotIndex) {
+				pivotIndex = j;
+			} else if (j === pivotIndex) {
+				pivotIndex = i;
+			}
+			swap(arr, i, j);
+		}
+	}
+
+	console.log(`arr: , ${arr}`);
+	console.log(`low: , ${low}`);
+	console.log(`pivotIndex: , ${pivotIndex}`);
+	console.log(`high: , ${high}`);
+
+
+	quickSort(arr, low, pivotIndex-1);
+	quickSort(arr, pivotIndex+1, high);
 }
+
+function swap(arr, lowIndex, highIndex) {
+	let temp = arr[lowIndex];
+	arr[lowIndex] = arr[highIndex];
+	arr[highIndex] = arr[lowIndex];
+}
+
 
 function bubbleSort(arr) {
-    let sorted = false;
+	let sorted = false;
 
-    while(!sorted) {
-        sorted = true;
+	while (!sorted) {
+		sorted = true;
 
-        for(let i = 0; i < arr.length - 1; i++) {
-            bCounter++;
+		for (let i = 0; i < arr.length - 1; i++) {
+			bCounter++;
 
-            if(arr[i] > arr[i+1]) {
-                let temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-                sorted = false;
-            }
-        }
-    }
+			if (arr[i] > arr[i + 1]) {
+				let temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				sorted = false;
+			}
+		}
+	}
 
-    return arr;
+	return arr;
 }
+
 
 function buildRandArr(len) {
-    let tempArr = [];
+	let tempArr = [];
 
-    for(let i = 0; i < len; i++) {
-        tempArr.push( Math.floor( Math.random() * 100 ) + 1 );
-    }
+	for (let i = 0; i < len; i++) {
+		tempArr.push(Math.floor(Math.random() * 100) + 1);
+	}
 
-    return tempArr;
+	return tempArr;
 }
+
 
 
 const testArr = buildRandArr(10);
+
+console.log("Test array: ", testArr);
 
 quickSort(testArr);
 console.log("Quick counter: ", qCounter);
